58cea9be25c3b5516dd105a50d659a05765200e7
Loukik3 <loukik.a@exactspace.co>
2024-03-14 18:44:44 +0000

    removed pa hdr

--- reqq.py
+++ reqq.py
@@ -2,14 +2,14 @@ import requests
 import json
 import time
 sleepTime = 2
-baseUrl = "http://10.36.141.34:8082"
-# baseUrl = "http://10.36.44.48:8082"
+# baseUrl = "http://10.36.141.34:8082"
+baseUrl = "http://10.36.44.48:8082"
 apiName = "/apcmanager/unitapc"
 
 wholeUrl = baseUrl + apiName
 
 body = {"unitsIdList":["63349b9c749f3c3081c6a472","635bb88abbf1fe000756dbfb"],"timeType":"daily"}#lpg5
-body = {"unitsIdList":["635b6f9fef6a59000703f924"],"timeType":"daily"}#hrd3
+# body = {"unitsIdList":["635b6f9fef6a59000703f924"],"timeType":"daily"}#hrd3
 res = requests.post(wholeUrl,json=body).json()
 print(res)
 if type(res) == list:
