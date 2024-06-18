045cf4bc5637db02ecc5356cf7b767ba547b3494
Loukik Adole <loukik@Loukiks-MacBook-Air.local>
2024-06-18 12:25:36 +0530

    JSW specific conditions

--- reqq.py
+++ reqq.py
@@ -5,13 +5,15 @@ sleepTime = 4
 baseUrl = "http://10.36.141.34:8082"
 # baseUrl = "http://10.36.44.48:8082"
 baseUrl = "http://10.0.0.14:8082"
+baseUrl = "http://127.0.0.1:8082"
 apiName = "/apcmanager/unitapc"
 
 wholeUrl = baseUrl + apiName
 
 # body = {"unitsIdList":["63349b9c749f3c3081c6a472","635bb88abbf1fe000756dbfb"],"timeType":"daily"}#lpg5
 body = {"unitsIdList":["635b6f9fef6a59000703f924"],"timeType":"daily"}#hrd3
-body = {"unitsIdList":["65817c16dcd5de0007bbbf5d"],"timeType":"daily"}#hrd3
+body = {"unitsIdList":["6375e28c32ebf700068ac0aa"],"timeType":"daily"}#hrd3
+
 res = requests.post(wholeUrl,json=body).json()
 print(res)
 if type(res) == list:
